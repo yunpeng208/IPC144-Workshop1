28b429ae32cfafb5e14c850e20f81afcb793c341
Yunpeng Song <monicafccsc@gmail.com>
2019-08-05 15:08:35 -0400

    new post
    IPC144 Fall2018 WS1

---
+++ w1_home.c
@@ -0,0 +1,13 @@
+/*
+Name: Yunpeng Song
+Student number: 155695174
+Email: ysong102@myseneca.ca
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("********************************\n"); // print all stars 
+	printf("*** Welcome to C Programming ***\n"); // print string literal  
+	printf("********************************\n"); // print all stars
+	return 0;
+}
---
+++ w1_lab.c
@@ -0,0 +1,12 @@
+/*
+Name: Yunpeng Song
+Student number: 155695174
+Email: ysong102@myseneca.ca
+Workshop: w1
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("*** Welcome to C Programming ***\n");
+	return 0;
+}
